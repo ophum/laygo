95bc6bde7b02479e5e5111bbcc323991df6a54e1
ophum <inagaki0106@gmail.com>
2022-10-01 18:36:53 +0900

    pkg laygo

---
+++ pkg/laygo/laygo.go
@@ -0,0 +1,24 @@
+package laygo
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/ophum/laygo/pkg/controllers"
+)
+
+func Run(addr string, controllers []controllers.Controller) error {
+	r := gin.Default()
+	RegisterRoutes(r, controllers)
+	return r.Run(addr)
+}
+
+func RunTLS(addr, certFile, keyFile string, controllers []controllers.Controller) error {
+	r := gin.Default()
+	RegisterRoutes(r, controllers)
+	return r.RunTLS(addr, certFile, keyFile)
+}
+
+func RegisterRoutes(router gin.IRouter, controllers []controllers.Controller) {
+	for _, controller := range controllers {
+		controller.RegisterRoutes(router)
+	}
+}
